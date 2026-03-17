e4b785ac3c18ce19bd08af3edc40fc06a21b3ccf
K3tty5555 <K3tty5555@users.noreply.github.com>
2026-03-17 23:24:23 +0800

    feat: remove ProjectStageBar, add sidebar to AppLayout, simplify TitleBar

--- app/src/layouts/AppLayout.tsx
+++ app/src/layouts/AppLayout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom"
+import { SidebarShell } from "@/components/layout/SidebarShell"
 import { TitleBar } from "@/components/layout/TitleBar"
 
 export function AppLayout() {
   return (
     <div className="flex h-screen flex-col overflow-hidden">
       <TitleBar />
-      <main className="flex-1 overflow-y-auto p-8">
-        <Outlet />
-      </main>
+      <div className="flex flex-1 overflow-hidden">
+        <SidebarShell />
+        <main className="flex-1 overflow-y-auto p-8">
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
